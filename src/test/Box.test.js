eb36a19b3cf17f5db90404b3504ef07c6300d267
MatiasDBrites <matias.brites@gmail.com>
2022-09-22 01:41:15 -0500

    fallaras

--- src/test/Box.test.js
+++ src/test/Box.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import Box from '../components/Box';
 
 test("Hay un contador en box", () => {
-  render(<Box />);
+  // render(<Box />);
   const itemContador = screen.getByText(/Contador/i)
   expect(itemContador).toBeInTheDocument();
 });
